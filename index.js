27c20bda0d71fb24ca79b63a8ff49914a25be010
helloworld-foo-bar <baldeaglekid@gmail.com>
2022-03-15 10:36:35 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+// run `node index.js` in the terminal
+
+console.log(`Hello Node.js v${process.versions.node}!`);
+
+try {
+  console.log(x);
+} catch {
+  console.log('Variable X is not defined!');
+}
